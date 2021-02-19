3f252feb8e7fd1f2f677e3866d18a69be9937dfe
treebat1 <50926050+treebat1@users.noreply.github.com>
2021-02-19 10:50:03 -0800

    Update ViewExtendedEventData.sql

--- ViewExtendedEventData.sql
+++ ViewExtendedEventData.sql
@@ -4,5 +4,4 @@ SELECT top 20 *
 FROM sys.fn_xe_file_target_read_file('C:\Temp\*.xel', null , null, null)
 --where event_data like ('%view%')
 --and event_data like '%strategy%'
---and event_data like '%uniqlo%'
 set nocount off
